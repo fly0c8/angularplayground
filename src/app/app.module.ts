196094fcbc8d0a674c1be80be131a883e564a31b
Arnold <arnold.angerer@gmail.com>
2018-11-27 20:43:45 +0100

    primeng table containing checkbox demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { TableModule } from "primeng/table";
+import { CheckboxModule } from "primeng/checkbox";
+import { TableTestComponent } from './table-test/table-test.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableTestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    TableModule,
+    CheckboxModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
